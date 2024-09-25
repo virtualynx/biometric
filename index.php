2d056b8cffa24f12adabc843dfef0067b22d1308
virtualynx <eryandi.angga.pratama@gmail.com>
2024-09-25 13:35:15 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Author: Dahir Muhammad Dahir
+ * Date: 12-April-2020 5:07 AM
+ * About: I will tell you later
+ */
+
+namespace fingerprint;
+
+// require("./src/html/home.html");
+require("./src/html/home2.html");
