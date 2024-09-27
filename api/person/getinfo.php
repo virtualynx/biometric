4360f3c6d8b4a9e5e8ccac073c4a38e5fcd3bd72
virtualynx <eryandi.angga.pratama@gmail.com>
2024-09-27 10:14:18 +0700

    change getinfo endpoint to post

--- api/person/getinfo.php
+++ api/person/getinfo.php
@@ -3,14 +3,14 @@ require_once(dirname(__FILE__)."/../../src/core/models/PersonModel.php");
 
 use biometric\src\core\models\PersonModel;
 
-if(empty($_GET['nik'])){
+if(empty($_POST['nik'])){
     http_response_code(400);
     echo 'Missing NIK';
     exit;
 }
 
 $pm = new PersonModel();
-$person = $pm->getInfo($_GET['nik']);
+$person = $pm->getInfo($_POST['nik']);
 
 if(empty($person)){
     echo 'Data not found';
