725c81cb4f06ff708e227841ae742ec6ed116882
virtualynx <eryandi.angga.pratama@gmail.com>
2024-10-02 11:22:26 +0700

    add api for master/activity.php and master/doc_check.php

---
+++ api/master/activity.php
@@ -0,0 +1,16 @@
+<?php
+
+use biometric\src\core\Database;
+
+require_once(dirname(__FILE__)."/../_api_header.php");
+require_once(dirname(__FILE__)."/../../src/core/Database.php");
+
+$db = new Database();
+
+try{
+    $res = $db->query("select * from master_act order by `order`");
+    echo json_encode($res);
+}catch(\Exception $e){
+    echo $e->getMessage();
+    exit;
+}
