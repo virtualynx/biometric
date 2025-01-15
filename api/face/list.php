330bd67a289200ef86e9799c64505341541fca8a
virtualynx <eryandi.angga.pratama@gmail.com>
2025-01-15 10:11:53 +0700

    add filter sk_number in endpoint api/face/list

--- api/face/list.php
+++ api/face/list.php
@@ -1,16 +1,32 @@
 <?php
 
 use biometric\src\core\models\FaceModel;
+use biometric\src\core\models\PersonModel;
 
 require_once(dirname(__FILE__)."/../_api_header.php");
 require_once(dirname(__FILE__)."/../../src/core/models/FaceModel.php");
+require_once(dirname(__FILE__)."/../../src/core/models/PersonModel.php");
 // require_once(dirname(__FILE__)."/../../src/core/helpers/helpers.php");
 
 $requestBody = file_get_contents('php://input');
 $json = json_decode($requestBody);
 
+$faces = [];
 $fm = new FaceModel();
-$faces = $fm->list($json->person_ids);
+$person_ids = [];
+if(!empty($json->person_ids)){
+    $person_ids = $json->person_ids;
+}else if(!empty($json->sk_number)){
+    $pm = new PersonModel();
+    $persons = $pm->list();
+    foreach($persons as $row){
+        if($row->sk_number == $json->sk_number){
+            $person_ids []= $row->nik;
+        }
+    }
+}
+
+$faces = $fm->list($person_ids);
 
 echo json_encode([
     'status' => 'success',
